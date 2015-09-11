95c29d42cbb8b37a8021a7864dd9ccd4190ab320
bchau <bchau93@hotmail.com>
2015-09-11 10:33:37 -0700

    Sorted Array of student objects

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ and open the template in the editor.
         $second->add_grade(80);
         $second->add_grade(50);
         $students['a456'] = $second;
-        
+        ksort($students);
         foreach ($students as $student)
             echo $student->toString();
         //echo "Hello...is anyone out there...";
