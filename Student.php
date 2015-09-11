c78d76972cee3d08d972a1729eff552ba69214fa
bchau <bchau93@hotmail.com>
2015-09-11 11:16:19 -0700

    Rounded Average Grade

--- Student.php
+++ Student.php
@@ -35,7 +35,7 @@ class Student {
         $total = 0;
         foreach ($this->grades as $value)
             $total += $value;
-        return $total / count($this->grades);
+        return round($total / count($this->grades));
         
     }
     
